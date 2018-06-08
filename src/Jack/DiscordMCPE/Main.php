c248dab56c4c56aefddd4e17b3d6a9c61f364556
Jack Honour <gangnam253@gmail.com>
2018-06-08 18:32:52 +0100

    Remove args

--- src/Jack/DiscordMCPE/Main.php
+++ src/Jack/DiscordMCPE/Main.php
@@ -206,7 +206,7 @@ class Main extends PluginBase implements Listener{
             "username" => $name
         ];
 
-        $this->getServer()->getAsyncPool()->submitTask(new tasks\SendAsync($player, $webhook, serialize($curlopts), $this->getServer()));
+        $this->getServer()->getAsyncPool()->submitTask(new tasks\SendAsync($player, $webhook, serialize($curlopts)));
 	return true;
     }
 }
