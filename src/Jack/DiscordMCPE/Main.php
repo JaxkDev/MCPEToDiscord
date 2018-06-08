9cf49840cd974de391ccb566817cd51931924c5f
Jack Honour <gangnam253@gmail.com>
2018-06-08 18:09:35 +0100

    Update Main.php

--- src/Jack/DiscordMCPE/Main.php
+++ src/Jack/DiscordMCPE/Main.php
@@ -206,7 +206,7 @@ class Main extends PluginBase implements Listener{
             "username" => $name
         ];
 
-        $this->getPlugin()->getScheduler()->scheduleAsyncTask(new tasks\SendAsync($player, $webhook, serialize($curlopts)));
+        $this->getScheduler()->scheduleAsyncTask(new tasks\SendAsync($player, $webhook, serialize($curlopts)));
         return true;
     }
 }
